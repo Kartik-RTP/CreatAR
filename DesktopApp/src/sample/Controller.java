d73982d3ac81a0cf325f3623eb7a4919f8a1d8e8
Rajat k <rj_rajat18@yahoo.in>
2016-10-08 10:04:54 +0530

    Adds First window files

--- DesktopApp/src/sample/Controller.java
+++ DesktopApp/src/sample/Controller.java
@@ -1,5 +1,4 @@
 package sample;
 
 public class Controller {
-//just a comment
 }
--- DesktopApp/src/sample/Main.java
+++ DesktopApp/src/sample/Main.java
@@ -10,9 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("New Project");
+
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
